f85c7e00ff2d6d751df18409792c4341d0d9e18b
Meleseabakiyacd <meleabakiya@gmail.com>
2022-12-23 13:08:04 -0800

    t10

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * _strcar - concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- * @dest@: string that will be appended
- * @stc: string to be concatenated upon
+ * _strcar - concatenates two string
+ * @dest: input value
+ * @src: input value
  *
- * Return: return poiner to @dest
+ * Return: void
  */
 char *_strcar(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
